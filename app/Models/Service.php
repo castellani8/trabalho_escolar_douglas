3aefb2f90d35dede52e189d386d253e11a6a5039
LorensCuscan <lorenscuscan@gmail.com>
2022-12-07 23:25:47 -0300

    created fillables for all models

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'fleet_id',
+        'service_desc',
+        'service_type_id',
+        'maintenance_type_id',
+    ];
 }
