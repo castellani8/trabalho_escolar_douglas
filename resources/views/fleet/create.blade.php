ed4a8ab5ebf8c7fefab8ecd569bc9bf3aa87fed0
LorensCuscan <lorenscuscan@gmail.com>
2022-12-14 05:01:55 -0300

    feat: bug fixes

--- resources/views/fleet/create.blade.php
+++ resources/views/fleet/create.blade.php
@@ -29,9 +29,9 @@
 
                     <label for="active">Ativo:</label>
                     <? let num = 1;
-                    let text = num.toString('Sim')
+                    let text = num.toString("Sim")
                     let num = 2;
-                    let text = num.toString('Não')> </?>
+                    let text = num.toString("Não")> </?>
                    <input class="form-control" placeholder="(Sim) | (Não)" value="" type="string" id="active" name="active" /> 
                 </div>
             </div>
